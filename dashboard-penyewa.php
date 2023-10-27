d39e8f8e1fea1055a82f5a378245cb4206ecd930
Bagus Anugrah <bagus.anugrah71@gmail.com>
2023-10-27 10:46:52 +0700

    mengganti format bootstrap

--- dashboard-penyewa.php
+++ dashboard-penyewa.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap5.css" rel="stylesheet">
 </head>
 <body>
 	<nav class="navbar navbar-expand-lg bg-secondary" data-bs-theme="dark">
@@ -102,6 +102,6 @@
 		</div>
 		<!-- Copyright -->
 	</footer>
-    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/bootstrap5.js"></script>
 </body>
 </html>
