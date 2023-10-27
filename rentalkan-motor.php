9e3252ea8962ab8716e7c74f7072e448b5414b93
Bagus Anugrah <bagus.anugrah71@gmail.com>
2023-10-27 13:09:24 +0700

    ubah title halaman

--- rentalkan-motor.php
+++ rentalkan-motor.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Rentalkan Motor</title>
     <link href="css/bootstrap5.css" rel="stylesheet">
 </head>
 <body>
