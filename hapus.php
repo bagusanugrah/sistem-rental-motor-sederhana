8e96ef706b978be6f9a63ed0507c00f5b6c3ccd9
Bagus Anugrah <bagus.anugrah71@gmail.com>
2023-10-30 05:18:35 +0700

    perbaikan autentikasi hapus motor

--- hapus.php
+++ hapus.php
@@ -4,8 +4,20 @@ include 'koneksi.php';
  
 //jalankan session
 session_start();
-//jika session username tidak diatur
+
+// menangkap data id yang di kirim dari url
+$plat_nomor = $_GET['idmotor'];
+//dapatkan data motor dari database
+$getmotor = mysqli_query($koneksi, "SELECT * FROM motor WHERE plat_nomor='$plat_nomor'");
+$motor = mysqli_fetch_array($getmotor);
+$id_pemilik = $motor['id_pemilik'];
+$merek = $motor['merek'];
+$tipe = $motor['tipe'];
+$biaya = $motor['sewa_perhari'];
 $username = '';
+
+
+//jika session username tidak diatur
 if(!isset($_SESSION['username'])){
     //redirect ke halaman login
     echo "<script>
@@ -14,6 +26,31 @@ if(!isset($_SESSION['username'])){
 }else{//jika session username diatur
     //nilai session username ditampung ke dalam variabel username
     $username = $_SESSION['username'];
+
+    //jika username pada query parameter id bukanlah pemilik yang login
+    if($id_pemilik != $username){
+        //redirect ke dashboard pemilik terkait
+        echo "<script>
+                document.location='dashboard-pemilik.php?id=$username';
+            </script>";
+    } else{//jika username pada query parameter id adalah pemilik yang login
+        // menghapus data dari database
+        $hapus = mysqli_query($koneksi,"delete from motor where plat_nomor='$plat_nomor'");
+
+        //jika berhasil hapus data motor
+        if($hapus){
+            //redirect ke dashboard pemilik
+            echo "<script>
+                    alert('Hapus data motor berhasil!');
+                    document.location='dashboard-pemilik.php?id=$username';
+                </script>";
+        } else{//jika gagal hapus data motor
+            echo "<script>
+                    alert('Hapus data motor gagal!');
+                    document.location='dashboard-pemilik.php?id=$username';
+                </script>";
+        }
+    }
 }
 
 //jika session role diatur dan session role bernilai 'penyewa'
@@ -26,38 +63,5 @@ if(isset($_SESSION['role']) && $_SESSION['role'] == 'penyewa'){
         </script>";
 }
 
-// menangkap data id yang di kirim dari url
-$plat_nomor = $_GET['idmotor'];
-//dapatkan data motor dari database
-$getmotor = mysqli_query($koneksi, "SELECT * FROM motor WHERE plat_nomor='$plat_nomor'");
-$motor = mysqli_fetch_array($getmotor);
-$id_pemilik = $motor['id_pemilik'];
-$merek = $motor['merek'];
-$tipe = $motor['tipe'];
-$biaya = $motor['sewa_perhari'];
-
-//jika pemilik yang login bukanlah pemilik motor yang dihapus datanya
-if($username != $id_pemilik){
-    //redirect ke dashboard pemilik terkait
-    echo "<script>
-            document.location='dashboard-pemilik.php?id=$username';
-        </script>";
-}
- 
-// menghapus data dari database
-$hapus = mysqli_query($koneksi,"delete from motor where plat_nomor='$plat_nomor'");
-
-//jika berhasil hapus data motor
-if($hapus){
-    //redirect ke dashboard pemilik
-    echo "<script>
-            document.location='dashboard-pemilik.php?id=$username';
-        </script>";
-} else{//jika gagal hapus data motor
-    echo "<script>
-            alert('hapus data motor gagal!');
-            document.location='dashboard-pemilik.php?id=$username';
-        </script>";
-}
  
 ?>
