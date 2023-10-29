e9dbf20792278e8a89cb7dd148eef77b714e1680
Bagus Anugrah <bagus.anugrah71@gmail.com>
2023-10-29 21:54:23 +0700

    mengganti  menjadi

--- tambah-motor.php
+++ tambah-motor.php
@@ -3,7 +3,7 @@
 include 'koneksi.php';
 
 //mengambil data dari form
-$iduser = $_POST['iduser'];
+$id_pemilik = $_POST['iduser'];
 $plat = strtoupper($_POST['plat']);
 $merek = $_POST['merek'];
 $tipe = $_POST['tipe'];
@@ -13,14 +13,14 @@ $biaya = $_POST['biaya'];
 if($plat == "" || $merek == ""|| $tipe == "" || $biaya == ""){
     echo "<script>
             alert('Form tidak boleh ada yang kosong!');
-            document.location='rentalkan-motor.php?id=$iduser';
+            document.location='rentalkan-motor.php?id=$id_pemilik';
         </script>";
 } else {//jika form tidak ada yang kosong
     //jika plat terdiri dari selain dari aphanumeric dan spasi
     if(!(ctype_alnum($plat) || strpos($plat, ' '))){
         echo "<script>
                 alert('plat tidak boleh selain dari huruf, angka, dan spasi!');
-                document.location='rentalkan-motor.php?id=$iduser';
+                document.location='rentalkan-motor.php?id=$id_pemilik';
             </script>";
     } else {//jika plat terdiri dari bukan selain alphanumeric
         $cekdata = mysqli_query($koneksi, "SELECT * FROM motor WHERE plat_nomor='$plat'");
@@ -28,24 +28,24 @@ if($plat == "" || $merek == ""|| $tipe == "" || $biaya == ""){
         //jika plat nomor belum terdaftar
         if(mysqli_num_rows($cekdata) == 0){
             //inputkan semua data dari form ke database
-            $simpan = mysqli_query($koneksi, "INSERT INTO motor(plat_nomor, merek, tipe, sewa_perhari, id_pemilik) VALUES('$plat', '$merek', '$tipe', '$biaya', '$iduser')");
+            $simpan = mysqli_query($koneksi, "INSERT INTO motor(plat_nomor, merek, tipe, sewa_perhari, id_pemilik) VALUES('$plat', '$merek', '$tipe', '$biaya', '$id_pemilik')");
 
             //jika berhasil inputkan data ke database
             if($simpan){
                 //redirect ke dashboard pemilik
                 echo "<script>
-                        document.location='dashboard-pemilik.php?id=$iduser';
+                        document.location='dashboard-pemilik.php?id=$id_pemilik';
                     </script>";
             } else{//jika gagal inputkan data ke database
                 echo "<script>
                         alert('Proses rentalkan motor gagal!');
-                        document.location='rentalkan-motor.php?id=$iduser';
+                        document.location='rentalkan-motor.php?id=$id_pemilik';
                     </script>";
             }
         } else{
             echo "<script>
                         alert('Plat nomor sudah ada!');
-                        document.location='rentalkan-motor.php?id=$iduser';
+                        document.location='rentalkan-motor.php?id=$id_pemilik';
                     </script>";
         }
     }
